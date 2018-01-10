d468105fab8f77f85bf2c6ed7c072ffdb741b1ad
Alyssa Kim <alyssa.kim@gmail.com>
2018-01-10 15:40:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,6 @@ function addElementToEndOfArray(array, element){
 function destructivelyAddElementToEndOfArray(array, element){
   return array.push(element);
 }
+function accessElementInArray(array, index){
+  return array[index];
+}
